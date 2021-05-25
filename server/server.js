8fdc2fe8defe1e123264d8637c05921f7a886483
Ian <isford7@gmail.com>
2021-05-25 17:07:17 -0500

    Got SQL database created, and info saved in database file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+//require pg and configuration for it
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool ({
+    database: 'jazzy_sql',
+    host: 'localhost',
+    port: 5432,
+})
+
+pool.on('connect', () => {
+    console.log('Connected to POSTGRES');
+});//test for DB connection
 
 const app = express();
 const PORT = 5000;
@@ -12,41 +24,41 @@ app.listen(PORT, () => {
 });
 
 // TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
+// const artistList = [ 
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },       
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
+// const songList = [
+//     {
+//         title: 'Take Five',
+//         length: '5:24',
+//         released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         released: '1959-08-17'
+//     },
+//     {
+//         title: 'Black Gold',
+//         length: '5:17',
+//         released: '2012-02-01'
+//     }
+// ];
 
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);
@@ -69,3 +81,4 @@ app.post('/song', (req, res) => {
 });
 
 
+app.post ('/',)
